345d1f9bb266b948ba65ffa40f465c7a57e031c2
Travis Fischer <fisch0920@gmail.com>
2023-03-13 08:56:10 -0500

    😹

--- docs/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './theme.config.tsx'],
-  theme: {
-    extend: {}
-  },
-  plugins: [],
-  darkMode: 'class'
-}
